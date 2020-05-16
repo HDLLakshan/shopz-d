368780d91e5a0fe55e40fc0bd6240019a103b41c
RaveenD <51952105+RaveenD@users.noreply.github.com>
2020-05-16 20:16:27 +0530

    Raveen Dissnayaka

--- src/Component/Admin/Admin.js
+++ src/Component/Admin/Admin.js
@@ -13,7 +13,7 @@ import {HomeAdmin} from "./HomeAdmin/HomeAdmin";
 
 const store = createStore(allReducers,
     compose(applyMiddleware(thunk),
-       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+     //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     ));
 store.dispatch(fetchAllPMs());
 store.dispatch(fetchAllCats());
